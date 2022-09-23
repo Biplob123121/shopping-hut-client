f4a2af1220fc8acae461431f9ee4df3d3bdfc106
Biplob123121 <biplobkb123@gmail.com>
2022-09-23 10:31:32 +0600

    modified sign in page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
  env : {
   "BASE_URL" : "http://localhost:3000",
-  "MONGODB_URL" : "mongodb+srv://shopping_hut_admin:jifR2KbRo5T8az1r@cluster0.hx8udxs.mongodb.net/shopping-hut"
+  "MONGODB_URL" : "mongodb+srv://shopping_hut_admin:jifR2KbRo5T8az1r@cluster0.hx8udxs.mongodb.net/shopping-hut",
+  "ACCESS_TOKEN_SECRET": "6EfCHWQ0jT$#ke^19BQfG2s9KJMxdlPGBLlLwd3CK96OjAfw",
+  "REFRESH_TOKEN_SECRET": "rUK#b32Fbe2^ZLN$!EjS0yRyNMShKkHx&m9GZ@Nk47rC%EXV"
  }
 }
 
